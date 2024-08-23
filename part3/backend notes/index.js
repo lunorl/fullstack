27673492fff0ffc06264fb5cbae85fb5212f9be4
lunorl <nicolas.huot@videotron.ca>
2024-08-23 17:50:32 -0400

    Update index.js

--- part3/backend notes/index.js
+++ part3/backend notes/index.js
@@ -24,30 +24,22 @@ const generateId = () => {
   app.post('/api/notes', (request, response) => {
     const body = request.body
   
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
-  
-    const note = {
+    const note = new Note({
       content: body.content,
-      important: Boolean(body.important) || false,
-      id: generateId(),
-    }
+      important: body.imortant || false
+    })
   
-    notes = notes.concat(note)
-  
-    response.json(note)
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
   })  
   app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
-    response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
   })
   
   app.get('/api/notes', (request, response) => {
