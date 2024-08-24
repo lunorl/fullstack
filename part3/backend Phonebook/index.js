4dc73f337f5478dbad0312731e3d8c073ea96c52
lunorl <nicolas.huot@videotron.ca>
2024-08-24 14:01:15 -0400

    making the rest of the operations work with the database

--- part3/backend Phonebook/index.js
+++ part3/backend Phonebook/index.js
@@ -6,74 +6,65 @@ const Person = require('./models/book')
 app.use(express.static('build'))
 app.use(cors())
 morgan('tiny')
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.use(express.json())
-
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  // this has to be the last loaded middleware, also all the routes should be registered before this!
+  app.use(errorHandler)
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(people => {
         response.json(people)
     })
 })
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person =>
         response.json(person)
-    )
-    const id = request.params.id
-    const person = persons.filter(book => book.id === id)
-    if (person.length === 0) {
-        response.status(404).end()
-    } else {
-        response.json(person)
-    }
+    ).catch(error => next(error))
 })
 app.get('/info', (request, response) => {
-    response.send(`<div>Phonebook has input for ${persons.length} people <br /> ${Date()}</div>`)
+    Person.countDocuments().then(count =>
+        response.send(`<div>Phonebook has input for ${count} people <br /> ${Date()}</div>`)
+    )
+})
+app.put('/api/persons/:id', (request, response) => {
+    body = request.body 
+    const person = {
+        name: body.content,
+        number: body.number,
+      }
+    Person.findByIdAndUpdate(request.params.id, person, {new: true}).then(person =>
+        response.json(person)
+    )
 }) 
 app.post('/api/persons', (request, response) => {   
-    const id = Math.floor(Math.random()*100)
-    const person = request.body
-    const final = {...person, "id": `${id}`}
-    const names = persons.map(person => person.name)
-    if (!final.name || !final.number) {
-        return response.status(400).json({
-            error: 'a part of the body is missing'
-        })
-    } else if (names.includes(final.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-    persons = persons.concat(final)
-    response.json(final)
-})
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
+    const body = request.body
+    if (body.name === undefined || body.number === undefined) {
+        return response.status(400).json({ error: 'content missing' })
+    } else {
+        const person = new Person({
+            name: body.name,
+            number: body.number,
 
-    response.status(204).end()
+        })
+        person.save().then(person => {
+            response.json(person)
+        })
+
+        }
+    }
+)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id).then(person =>
+        response.status(204).end()
+    ).catch(error => next(error))
 })
 const PORT = process.env.PORT || 3001
 app.listen(PORT, '0.0.0.0',  () => {
--- part3/backend notes/index.js
+++ part3/backend notes/index.js
@@ -10,16 +10,21 @@ console.log('hi');
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
-let notes = [  {    id: "1",    content: "HTML is easy",    important: true  },  {    id: "2",    content: "Browser can execute only JavaScript",    important: false  },  {    id: "3",    content: "GET and POST are the most important methods of HTTP protocol",    important: true  }]
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => Number(n.id)))
-      : 0
-    return String(maxId + 1)
-  }
   
   app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -36,10 +41,15 @@ const generateId = () => {
       response.json(savedNote)
     })
   })  
-  app.get('/api/notes/:id', (request, response) => {
+  app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-      response.json(note)
+      if (note) {        
+        response.json(note)      
+      } else {        
+        response.status(404).end()      
+      }    
     })
+    .catch(error => next(error))
   })
   
   app.get('/api/notes', (request, response) => {
@@ -49,13 +59,27 @@ const generateId = () => {
       
     })
   })
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
+  app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
   })
-  
+  app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id).
+    then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+  })
   const PORT = process.env.PORT || 3001
   app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`)
