78b923a3353c28c936e3f7fb8eb2729ec99af136
lunorl <nicolas.huot@videotron.ca>
2024-08-31 15:27:40 -0400

    complete unit tests

--- part4/Backend blog/tests/test_1.test.js
+++ part4/Backend blog/tests/test_1.test.js
@@ -58,3 +58,102 @@ describe('total likes', () => {
     assert.strictEqual(result, 63)
   })
 })
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f2',
+      title: 's',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 3,
+      __v: 1
+    },
+    {
+      _id: '5a422aa71b54a676234d17f1',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 55,
+      __v: 2
+    }
+  ]
+
+  test('when list has a lot and get fav', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.deepStrictEqual(result, listWithOneBlog[2])
+  })
+})
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Blammmo',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f2',
+      title: 's',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 3,
+      __v: 1
+    },
+    {
+      _id: '5a422aa71b54a676234d17f1',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 55,
+      __v: 2
+    }
+  ]
+
+  test('when list has a lot and get fav', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    assert.deepStrictEqual(result, 'Edsger W. Dijkstra')
+  })
+})
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Blammmo',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f2',
+      title: 's',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 3,
+      __v: 1
+    },
+    {
+      _id: '5a422aa71b54a676234d17f1',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 55,
+      __v: 2
+    }
+  ]
+
+  test('when list has a lot and get fav', () => {
+    const result = listHelper.mostLikes(listWithOneBlog)
+    assert.deepStrictEqual(result, {author: 'Edsger W. Dijkstra', likes: 58})
+  })
+})
--- part4/Backend blog/utils/list_helper.js
+++ part4/Backend blog/utils/list_helper.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const dummy = (blogs) => {
     return 1
 }
@@ -6,6 +7,27 @@ const totalLikes = (blogs) => {
     blogs.forEach(blog => likes += blog.likes)
     return likes
 }
+const favoriteBlog = (blogs) => {
+    let highest = 0
+    let returned = null
+    blogs.forEach(blog => {
+        if (blog.likes > highest) {
+            returned = blog
+            highest = blog.likes
+        }
+    })
+    return returned
+}
+const mostBlogs = (blogs) => {
+    const long = _.groupBy(blogs, 'author')
+    const longest = _.maxBy(Object.keys(long), (author) => long[author].length)
+    return longest
+}
+const mostLikes = (blogs) => {
+    const long = _.groupBy(blogs, 'author')
+    const longest = _.maxBy(Object.keys(long), (author) => long[author].reduce((sum, blog) => sum+blog.likes, 0))
+    return {author: longest, likes: long[longest].reduce((sum, blog) => sum+blog.likes, 0)}
+}
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
