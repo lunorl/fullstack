06e244d8386ed803d14fae8127010c50655a19e7
lunorl <nicolas.huot@videotron.ca>
2024-09-23 16:33:14 -0400

    redux with json server

--- part6/tutorial app/src/App.jsx
+++ part6/tutorial app/src/App.jsx
@@ -1,9 +1,17 @@
-import { createNote, toggleImportanceOf} from './reducers/noteReducer'
-import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 import NewNote from './NewNote'
 import Notes from './Notes'
 import VisibilityFilter from './VisibilityFilter'
+import noteService from '../services/notes'
+import { setNotes} from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
+
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    noteService
+    .getAll().then(notes => dispatch(setNotes(notes)))
+  }, [])
   return (
     <div>
       <NewNote />
--- part6/tutorial app/src/main.jsx
+++ part6/tutorial app/src/main.jsx
@@ -4,10 +4,11 @@ import App from './App'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
-import noteReducer from './reducers/noteReducer'
+import noteReducer, { setNotes } from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 import { createNote } from './reducers/noteReducer'
 import { filterChange } from './reducers/filterReducer'
+import noteService from '../services/notes'
 
 const store = configureStore({
   reducer: {
@@ -16,6 +17,8 @@ const store = configureStore({
   }
 })
 
+noteService.getAll().then(notes => (setNotes(notes))
+)
 console.log(store.getState())
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
