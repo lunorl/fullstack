8455c8f6ce47d9528587e58d99df1868126c2bbd
lunorl <nicolas.huot@videotron.ca>
2024-09-28 15:15:21 -0400

    State handling

--- part6/tutorial app/src/main.jsx
+++ part6/tutorial app/src/main.jsx
@@ -1,25 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import App from './App'
-import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
-
-import noteReducer, { setNotes } from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
-import noteService from '../services/notes'
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
-
-noteService.getAll().then(notes => (setNotes(notes))
-)
-console.log(store.getState())
+import store from './store'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
--- part6/tutorial app/src/reducers/noteReducer.js
+++ part6/tutorial app/src/reducers/noteReducer.js
@@ -1,3 +1,4 @@
+import noteService from '../../services/notes'
 import { createSlice, current } from '@reduxjs/toolkit'
 
 const initialState = [
@@ -44,5 +45,18 @@ const noteSlice = createSlice({
     }
   }
 })
-export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+export const { toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+
+export const initializeNotes = () => {
+  return async dispatch => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+export const createNote = content => {
+  return async dispatch => {
+    const newNote = await noteService.createNew(content)
+    dispatch(appendNote(newNote))
+  }
+}
 export default noteSlice.reducer
